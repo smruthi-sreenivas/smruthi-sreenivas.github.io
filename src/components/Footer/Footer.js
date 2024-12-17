00e968820f01cb8833e7a0ddd94945eab1c65822
Smruthi S <157623467+smruthi-sreenivas@users.noreply.github.com>
2024-12-17 22:53:05 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,12 +2,9 @@ import './Footer.css'
 
 const Footer = () => (
   <footer className='footer'>
-    <a
-      href='https://github.com/smruthi-sreenivas/smruthi-sreenivas.github.io/'
-      className='link footer__link'
-    >
+    
       Created By Smruthi S
-    </a>
+    
   </footer>
 )
 
