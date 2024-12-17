d085ab688dc8702eeb97c0e27e040f1bb2613ef4
Smruthi S <157623467+smruthi-sreenivas@users.noreply.github.com>
2024-12-17 21:46:44 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
+  homepage: 'https://smruthi-sreenivas.github.io/',
   title: 'JS.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Smruthi S',
+  role: 'Computer Vision Engineer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'I am a Dynamic Computer Vision Engineer with a strong background in developing advanced Deep Learning models and image processing techniques. I specialize in using PyTorch and OpenCV, with a proven track record of achieving high accuracy in competitive settings.',
+  resume: 'https://drive.google.com/file/d/1KwMAtTyqNTAOefditUECxWdF0enhOL-H/view?usp=drive_link',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/smruthisreenivas',
+    github: 'https://github.com/smruthi-sreenivas',
   },
 }
 
@@ -21,51 +21,58 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Kenyan Food Classifier',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
+      'Classify image into any one of the 13 Kenyan food classes.',
+    stack: ['Image Classification', 'Deep Learning', 'PyTorch', 'OpenCV'],
+    sourceCode: 'https://github.com/smruthi-sreenivas/Kenyan_food_classifier',
+    livePreview: 'https://github.com/smruthi-sreenivas/Kenyan_food_classifier',
+  },
+  {
+    name: 'Aerial Drone Segmentation',
+    description:
+      'Aerial Drone Segmentation for Precise Landscape Analysis',
+    stack: ['Image Segmentation', 'Deep Learning', 'PyTorch', 'OpenCV'],
+    sourceCode: 'https://github.com/smruthi-sreenivas/Kenyan_food_classifier',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Project 2',
+    name: 'Object Detection and Tracking',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Detect and track a sports ball in a video',
+    stack: ['Object Detection', 'Object Tracking', 'OpenCV', 'Python'],
+    sourceCode: 'https://github.com/smruthi-sreenivas/Detection-and-Tracking',
+    livePreview: 'https://github.com/smruthi-sreenivas/Detection-and-Tracking',
   },
   {
-    name: 'Project 3',
+    name: 'Sunglass Filter',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Virtual sunglass effect to real-time webcam footage using OpenCV and Python.',
+    stack: ['Image Processing', 'Python', 'OpenCV'],
+    sourceCode: 'https://github.com/smruthi-sreenivas/Sunglasses-Filter',
+    livePreview: 'https://github.com/smruthi-sreenivas/Sunglasses-Filter',
   },
 ]
 
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
-  'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'Computer Vision',
+  'Deep Learning',
+  'Image Classification',
+  'Object Detection',
+  'Image Segmentation',
+  'Obect Tracking',
+  'Image Processing',
+  'OpenCV',
+  'PyTorch',
+  'Python',
+
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'smruthi.kukku.s@mail.com',
 }
 
 export { header, about, projects, skills, contact }
